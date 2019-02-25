679fe902539f90921cc1be5e5d8d14ac9f024019
Charis Ioannikios Kaonis <ch.kaonis@gmail.com>
2019-02-25 21:16:19 +0200

    Code cleanup Stage 1  💦

--- routes/hosts.js
+++ routes/hosts.js
@@ -1,129 +1,136 @@
 const sqlite3 = require('sqlite3').verbose();
 const wol = require('wake_on_lan');
-var express = require('express');
+const express = require('express');
 
-var router = express.Router();
+const router = express.Router();
 
-var db = new sqlite3.Database('./db/woly.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the WoLy database.');
+const db = new sqlite3.Database('./db/woly.db', err => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the WoLy database.');
 });
 
-
-var hostTable = [
-    ['whitehead', '50:E5:49:55:4A:8C', '192.168.1.10', 'asleep'],
-    ['phantom-senior', '00:24:8C:23:D6:3E', '192.168.1.7', 'asleep'],
-    ['phantom qualcomm', '40:8D:5C:53:90:91', '192.168.1.100', 'asleep'],
-    ['phantom intel', '40:8D:5C:53:90:93', '192.168.1.101', 'asleep'],
-    ['giota-pc', '74:2F:68:C8:BD:C5', '192.168.1.8', 'asleep'],
-    ['jb-ng', '50:E5:49:EF:26:DA', '192.168.1.49', 'asleep'],
-    ['jb-pc', '50:E5:49:56:4A:8C', '192.168.1.50', 'asleep']
+const hostTable = [
+  ['whitehead', '50:E5:49:55:4A:8C', '192.168.1.10', 'asleep'],
+  ['phantom-senior', '00:24:8C:23:D6:3E', '192.168.1.7', 'asleep'],
+  ['phantom qualcomm', '40:8D:5C:53:90:91', '192.168.1.100', 'asleep'],
+  ['phantom intel', '40:8D:5C:53:90:93', '192.168.1.101', 'asleep'],
+  ['giota-pc', '74:2F:68:C8:BD:C5', '192.168.1.8', 'asleep'],
+  ['jb-ng', '50:E5:49:EF:26:DA', '192.168.1.49', 'asleep'],
+  ['jb-pc', '50:E5:49:56:4A:8C', '192.168.1.50', 'asleep']
 ];
 
-router.get('/:name', function (req, res) {
-    var name = req.params.name;
-    console.log(`Retrieving host with name ${name}`);
+router.get('/:name', (req, res) => {
+  const { name } = req.params.name;
+  console.log(`Retrieving host with name ${name}`);
 
+  // SQL statement
+  const sql =
+    'SELECT hosts.name name, hosts.mac mac, hosts.ip ip, hosts.status status' +
+    'FROM hosts' +
+    'WHERE name  = ?';
 
-    //SQL statement
-    var sql = `SELECT hosts.name name, hosts.mac mac, hosts.ip ip, hosts.status status
+  // return first row only
+  db.get(sql, [name], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    if (!row) {
+      res.status(204).send();
+      return console.log(`No host found with the name ${name}`);
+      // console.log(`No host found with the name ${name}`);
+    }
+    res.json(row);
+    return console.log(`Found and sent host ${row.name} details`);
+  });
+});
+
+router.get('/wakeup/:name', (req, res) => {
+  const { name } = req.params.name;
+  console.log(`Trying to wake up host with name $name`);
+
+  // SQL statement
+  const sql = `SELECT hosts.name name, hosts.mac mac, hosts.ip ip, hosts.status status
            FROM hosts
            WHERE name  = ?`;
 
-
-    // return first row only
-    db.get(sql, [name], (err, row) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        if (!row) {
-            res.status(204).send();
-            return console.log(`No host found with the name ${name}`);
-            //console.log(`No host found with the name ${name}`);
-        }
-        res.json(row);
-        return console.log(`Found and sent host ${row.name} details`);
+  db.get(sql, [name], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    if (!row) {
+      res.status(204).send();
+      return console.log(`No host found with name ${name}`);
+      // return console.error(new Error(`No host found with the name ${name}`));
+    }
+    wol.wake(row.mac, error => {
+      if (error) {
+        res.send(`[{"name": ${row.name},"result": "error"}]`);
+        return console.error(`Error waking up host with name ${name} ${error.stack}`);
+      }
+      wol.createMagicPacket(row.mac);
+      console.log(`Sent WoL to host with name ${name}`);
+      res.send(`[{"name": ${row.name},"result": "success"}]`);
+      return null;
     });
+
+    // const magic_packet = wol.createMagicPacket(row.mac);
+    return row;
+  });
 });
-
-router.get('/wakeup/:name', function (req, res) {
-    var name = req.params.name;
-    console.log('Trying to wake up host with name ' + name);
-
-    //SQL statement
-    var sql = `SELECT hosts.name name, hosts.mac mac, hosts.ip ip, hosts.status status
-           FROM hosts
-           WHERE name  = ?`;
-
-    db.get(sql, [name], (err, row) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        if (!row) {
-            res.status(204).send();
-            return console.log(`No host found with name ${name}`);
-            //return console.error(new Error(`No host found with the name ${name}`));
-        }
-        wol.wake(row.mac, function (error) {
-            if (error) {
-                res.send('[{"name": ' + row.name + ',"result": "error"}]');
-                return console.error(`Error waking up host with name ${name}` + error.stack);
-            }
-            wol.createMagicPacket(row.mac);
-            console.log(`Sent WoL to host with name ${name}`);
-            res.send('[{"name": ' + row.name + ',"result": "success"}]');
-        });
-
-        //var magic_packet = wol.createMagicPacket(row.mac);
-        return row;
-    });
-});
-
 
 db.run(
-    'CREATE TABLE IF NOT EXISTS hosts(\n' +
+  'CREATE TABLE IF NOT EXISTS hosts(\n' +
     ' name text PRIMARY KEY UNIQUE,\n' +
     ' mac text NOT NULL UNIQUE,\n' +
     ' ip text NOT NULL UNIQUE,\n' +
     ' status text NOT NULL\n' +
     //    ' table_constraint\n' +
     ');',
-    function (err) {
-        for (var i = 0; i < hostTable.length; i++) {
-            db.run(`INSERT INTO hosts("name", "mac", "ip", "status") VALUES(?,?,?,?)`,
-                hostTable[i][0], hostTable[i][1], hostTable[i][2], hostTable[i][3],
-                function (err) {
-                    if (err) {
-                        return console.log(err.message);
-                    }
-                    console.log(`Row inserted ${this.changes}`);
-                });
+  err => {
+    for (let i = 0; i < hostTable.length; i += 1) {
+      db.run(
+        'INSERT INTO hosts("name", "mac", "ip", "status") VALUES(?,?,?,?)',
+        hostTable[i][0],
+        hostTable[i][1],
+        hostTable[i][2],
+        hostTable[i][3],
+        error => {
+          if (error) {
+            return console.log(error.message);
+          }
+          return console.log(`Row inserted ${this.changes}`);
         }
-        if (err) {
-            return console.log(err.stack);
-        }
-        //console.log(data.toString());
+      );
     }
+    if (err) {
+      return console.log(err.stack);
+    }
+    // console.log(data.toString());
+    return null;
+  }
 );
 
-router.get('/', function (req, res) {
-    var sql = `SELECT hosts.name name, hosts.mac mac, hosts.ip ip, hosts.status status
-           FROM hosts`;
+router.get('/', (req, res) => {
+  const sql = 'SELECT hosts.name name, hosts.mac mac, hosts.ip ip, hosts.status status FROM hosts';
 
-    var hostArray = [];
+  const hostArray = [];
 
-    db.each(sql, function (err, row) {
-        hostArray.push(row);
-    }, function () {
-        console.log(hostArray);
-        res.json(hostArray);
-    });
+  db.each(
+    sql,
+    (err, row) => {
+      hostArray.push(row);
+    },
+    () => {
+      console.log(hostArray);
+      res.json(hostArray);
+    }
+  );
 });
 /*
 exports.addHost = function (req, res) {
-    var host = req.body;
+    const host = req.body;
     console.log('Adding host: ' + JSON.stringify(host));
     db.collection('hosts', function (err, collection) {
         collection.insert(host, {safe: true}, function (err, result) {
@@ -139,8 +146,8 @@ exports.addHost = function (req, res) {
 */
 /*
 router.get('/update/:name exports.updateHost = function (req, res) {
-    var id = req.params.id;
-    var host = req.body;
+    const id = req.params.id;
+    const host = req.body;
     console.log('Updating host: ' + id);
     console.log(JSON.stringify(host));
     db.collection('hosts', function (err, collection) {
@@ -158,7 +165,7 @@ router.get('/update/:name exports.updateHost = function (req, res) {
 */
 /*
 exports.deleteHost = function (req, res) {
-    var id = req.params.id;
+    const id = req.params.id;
     console.log('Deleting host: ' + id);
     db.collection('hosts', function (err, collection) {
         collection.remove({'_id': new mongo.ObjectID(id)}, {safe: true}, function (err, result) {
@@ -176,11 +183,10 @@ exports.deleteHost = function (req, res) {
 // Populate database with sample data -- Only used once: the first time the application is started.
 // You'd typically not find this code in a real-life app, since the database would already exist.
 
-
 /*
-var populateDB = function() {
+const populateDB = function() {
 
-    var hosts = [
+    const hosts = [
 	{
       "name" : "whitehead",
       "mac" : "50:E5:49:55:4A:8C",
@@ -232,14 +238,13 @@ var populateDB = function() {
     };
 */
 
-
 /* sqlite
     db.close((err) => {
         if (err) {
             console.error(err.message);
         }
         console.log('Close the database connection.');
-    });*/
-
+    });
+*/
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-var express = require('express');
-var hosts = require('./routes/hosts');
+const express = require('express');
+const hosts = require('./routes/hosts');
 
-var app = express();
-//var fs = require("fs");
-//var router = express.Router();
+const app = express();
+// const fs = require("fs");
+// const router = express.Router();
 /*
 router.get('/hosts', hosts.findAll);
 router.get('/hosts/:name', hosts.findByName);
 router.get('/hosts/wakeup/:name', hosts.wakeUp);
 */
 
-app.use('/hosts', hosts);
+app.get('/hosts', hosts);
 
-var server = app.listen(8082, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+const server = app.listen(8082, () => {
+  const { host } = server.address().address;
+  const { port } = server.address().port;
 
-  console.log("WoLy listening at http://%s:%s", host, port);
-
+  console.log('WoLy listening at http://%s:%s', host, port);
 });
