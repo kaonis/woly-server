46c5c798f84eed351427fb8ff98021df0ad065df
kaonis <10783823+kaonis@users.noreply.github.com>
2025-11-19 02:03:50 +0200

    Refactor MAC vendor API rate limiting comment; update package dependencies and reorder route definitions

--- routes/hosts.js
+++ routes/hosts.js
@@ -13,12 +13,11 @@ router.post('/scan', hostsController.scanNetwork);
 // Add a new host manually
 router.post('/', hostsController.addHost);
 
-// Get a specific host by name
-router.get('/:name', hostsController.getHost);
-
 // Get MAC address vendor information
 router.get('/mac-vendor/:mac', hostsController.getMacVendor);
 
+// Get a specific host by name
+router.get('/:name', hostsController.getHost);
 // Wake up a specific host
 router.post('/wakeup/:name', hostsController.wakeUpHost);
 
